d18ba836095a9836ecee782ad6aaa6572b30f746
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-07-30 11:44:39 +0900

    feat: add autoware_prefix and update conversion rule
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- autoware_msg_bag_converter/converter.py
+++ autoware_msg_bag_converter/converter.py
@@ -17,22 +17,34 @@
 # https://github.com/ros2/rosbag2/blob/rolling/rosbag2_py/test/test_reindexer.py
 
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-from autoware_auto_control_msgs.msg import AckermannControlCommand
 from autoware_control_msgs.msg import Control
 from rclpy.serialization import deserialize_message
 from rclpy.serialization import serialize_message
 from rosbag2_py import Reindexer
 from rosbag2_py import TopicMetadata
 from rosidl_runtime_py.utilities import get_message
+from tier4_planning_msgs.msg import PathWithLaneId as T4PathWithLaneId
 
 from autoware_msg_bag_converter.bag import create_reader
 from autoware_msg_bag_converter.bag import create_writer
 from autoware_msg_bag_converter.bag import get_storage_options
 
+if TYPE_CHECKING:
+    from autoware_auto_control_msgs.msg import AckermannControlCommand
+    from autoware_auto_planning_msgs.msg import PathWithLaneId as AutoPathWithLaneId
+
 TYPES_NOT_SIMPLY_REPLACED = {
     "autoware_auto_control_msgs/msg/AckermannControlCommand": "autoware_control_msgs/msg/Control",
+    "autoware_auto_planning_msgs/msg/PathWithLaneId": "tier4_planning_msgs/msg/PathWithLaneId",
 }
+
+TYPES_TO_ADD_AUTOWARE_PREFIX = [
+    "control_validator/msg/ControlValidatorStatus",
+    "planning_validator/msg/PlanningValidatorStatus",
+    "vehicle_cmd_gate/msg/IsFilterActivated",
+]
 
 
 def change_topic_type(old_type: TopicMetadata) -> TopicMetadata:
@@ -40,6 +52,12 @@ def change_topic_type(old_type: TopicMetadata) -> TopicMetadata:
         return TopicMetadata(
             name=old_type.name,
             type=TYPES_NOT_SIMPLY_REPLACED[old_type.name],
+            serialization_format="cdr",
+        )
+    if old_type.name in TYPES_TO_ADD_AUTOWARE_PREFIX:
+        return TopicMetadata(
+            name=old_type.name,
+            type=f"autoware_{old_type.name}",
             serialization_format="cdr",
         )
     # If old_type is not in the conversion rules, simply remove "auto_" and use that as the new type.
@@ -61,10 +79,22 @@ def convert_msg(topic_name: str, msg: bytes, type_map: dict) -> bytes:
     )
     if old_type == "autoware_auto_control_msgs/msg/AckermannControlCommand":
         old_msg: AckermannControlCommand
-        return Control(
-            stamp=old_msg.stamp,
-            lateral=old_msg.lateral,
-            longitudinal=old_msg.longitudinal,
+        return serialize_message(
+            Control(
+                stamp=old_msg.stamp,
+                lateral=old_msg.lateral,
+                longitudinal=old_msg.longitudinal,
+            ),
+        )
+    if old_type == "autoware_auto_planning_msgs/msg/PathWithLaneId":
+        old_msg: AutoPathWithLaneId
+        return serialize_message(
+            T4PathWithLaneId(
+                header=old_msg.header,
+                points=old_msg.points,
+                left_bound=old_msg.left_bound,
+                right_bound=old_msg.right_bound,
+            ),
         )
     return None
 
